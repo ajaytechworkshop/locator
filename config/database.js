618896d5055d64a3ea7e788f37ded57b972fdebc
Ajay <ajaytechworkshop@gmail.com>
2019-08-10 13:32:10 +0200

    Chnaged mongo db connection string to connect to atlas mongo

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ const dbServer = 'localhost';
 const dbPort = '27017';
 const dbName = 'geolocator';
 
-mongoose.connect(`mongodb://${dbServer}:${dbPort}/${dbName}`);
+//mongoose.connect(`mongodb://${dbServer}:${dbPort}/${dbName}`);
 
-//mongoose.connect('mongodb+srv://mongouser:mongouser@mongoone-lz8dp.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://mongouser:mongouser@mongoone-lz8dp.mongodb.net/test?retryWrites=true&w=majority');
 
 module.exports = mongoose;
 
