cda6757b5db2e1d8b912274df31a2400f5d494d8
Ajay <ajaytechworkshop@gmail.com>
2019-08-11 20:00:23 +0200

    App Completed

--- config/database.js
+++ config/database.js
@@ -3,10 +3,11 @@ let mongoose =  require('mongoose');
 const dbServer = 'localhost';
 const dbPort = '27017';
 const dbName = 'geolocator';
+//Local Mongo connection String
+mongoose.connect(`mongodb://${dbServer}:${dbPort}/${dbName}`);
 
-//mongoose.connect(`mongodb://${dbServer}:${dbPort}/${dbName}`);
-
-mongoose.connect('mongodb+srv://mongouser:mongouser@mongoone-lz8dp.mongodb.net/test?retryWrites=true&w=majority');
+//Atlas Mongo db connection String
+//mongoose.connect('mongodb+srv://mongouser:mongouser@mongoone-lz8dp.mongodb.net/test?retryWrites=true&w=majority');
 
 module.exports = mongoose;
 
